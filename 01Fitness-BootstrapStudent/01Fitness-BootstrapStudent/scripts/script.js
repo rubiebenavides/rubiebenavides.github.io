35c558404b40628fab9134612df7c67482315ec6
Rubie Benavides <73657272+rubiebenavides@users.noreply.github.com>
2020-11-30 22:36:08 -0800

    Add files via upload
    Added Fitness-BootstrapStudent on 30 November 2020.

---
+++ 01Fitness-BootstrapStudent/01Fitness-BootstrapStudent/scripts/script.js
@@ -0,0 +1,12 @@
+/* 
+    Student Name: Rubie Benavides
+    File Name: script.js
+    Date: 30 November 2020
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height ($(window).height());{
+
+    }
+})
