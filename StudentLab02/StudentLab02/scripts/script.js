0f1ade108fae489eb92915f50a7db79cac5e8e0c
Rubie Benavides <73657272+rubiebenavides@users.noreply.github.com>
2020-12-02 16:20:38 -0800

    Add files via upload
    Added Wild Rescue on 2 December 2020.

---
+++ StudentLab02/StudentLab02/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Rubie Benavides
+    File Name: script.js
+    Date: 2 December 2020
+*/
+
+//jQuesry for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
