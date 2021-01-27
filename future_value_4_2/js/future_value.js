06c7194e24a7b295d458e0d5b07defbfc337c695
Rubie Benavides <73657272+rubiebenavides@users.noreply.github.com>
2021-01-26 21:32:07 -0700

    Add files via upload
    Added Exercise 4-2 on 26 January 2021.

---
+++ future_value_4_2/js/future_value.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const $ = selector => document.querySelector(selector);
+
+const calculateFV = (investment, rate, years) => {
+    let furtureValue = investment;
+    for (let i = 1; i <= years; i++) {
+        furtureValue += furtureValue * rate / 100;
+    }
+    furtureValue = furtureValue.toFixed(2);
+    return furtureValue;
+};
+
+/*const processEntries = () => {
+    const investment = parseFloat($("#investment").value);
+    const rate = parseFloat($("#rate").value);
+    const years = parseInt($("#years").value);
+    let errorMessage = "";
+}
+
+if (isNaN(investment) || investment <= 0 || investment > 100000) {
+    errorMessage = "Investment must be a number greater than or equal to 100,000";
+    $("#investment").focus();
+    
+}
+else if (isNaN(rate) || rate <= 0 || rate > 15) {
+    errorMessage = "Rate must be a number greater than or equal to 15";
+    $("#rate").focus();
+
+}
+else if (isNaN(year) || years <= 0 || years > 50) {
+    errorMessage = "Years must be a number greater than 0 and less than or equal to 50";
+    $("#years").focus();
+}
+if (errorMessage == "") {
+    $ ("#future_value").value = calculateFV(investment, rate, years);
+}
+else {
+    alert(errorMessage);
+}
+*/
+document.addEventListener("DOMContentLoaded", () => {
+    $("#calculate").addEventListener("click", () => {
+    const investment = parseFloat($("#investment").value);
+    const rate = parseFloat($("#rate").value);
+    const years = parseInt($("#years").value);
+    let errorMessage = "";
+
+    if (isNaN(investment) || investment <= 0 || investment > 100000) {
+        errorMessage = "Investment must be a number greater than 0 and less than 100,000";
+        $("#investment").focus;
+    }
+    else if (isNaN(rate) || rate <= 0 || rate > 15) {
+        errorMessage = "Rate must be a number greater than 0 and less than or equal to 50";
+        $("#rate").focus();
+    }
+    else if (isNaN(years) || years <=0 || years > 50) {
+        errorMessage = "Years must be a number greater than 0 and less than or equal to 50";
+        $("#years").focus();
+    }
+    if (errorMessage == "") {
+        $("#future_value").value = calculateFV(investment, rate, years);
+    }
+    else {
+        alert(errorMessage);
+    }
+});
+$("#investment").focus();
+    
+});
+
