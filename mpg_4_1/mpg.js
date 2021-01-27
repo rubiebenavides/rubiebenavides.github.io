f2a166c28d99dda599dae8ac22279533bfe77da4
Rubie Benavides <73657272+rubiebenavides@users.noreply.github.com>
2021-01-26 18:42:23 -0700

    Add files via upload
    Added Exercise 4-1 on 26 January, 2021.

---
+++ mpg_4_1/mpg.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const $ = selector => document.querySelector(selector);
+
+const getNumberErrorMsg = lbl => `${lbl} must be a valid.`;
+const getRangeErrorMsg = lbl => `${lbl} must be greater than zero.`;
+
+const focusAndSelect = selector => {
+    const elem = $(selector);
+    elem.focus();
+    elem.select();
+};
+
+const processEntries = () => {
+    const miles = parseFloat($("#miles").value);
+    const gallons = parseFloat($("#gallons").value);
+
+    if (isNaN(miles)) {
+        alert(getNumberErrorMsg("Miles driven"));
+        focusAndSelect("#miles");
+    }    
+    else if (miles <= 0) {
+    alert(getRangeErrorMsg("Miles driven"));
+    focusAndSelect("#miles");
+
+    } 
+    else if (isNaN(gallons)) {
+        alert(getNumberErrorMsg("Gallons of gas used"));
+        focusAndSelect("#gallons");
+        
+    } 
+    else if (gallons <= 0) {
+        alert(getRangeErrorMsg)("Gallons of gas used");
+        focusAndSelect("#gallons");
+    }
+    else {
+        $("#mpg").value = (miles / gallons).toFixed(2);
+    }
+};
+
+const clearEntries = () => {
+    $("#miles").value = "";
+    $("#gallons").value = "";
+    $("#mpg").value = "";
+    $("#miles").focus();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    $("#calculate").addEventListener("click", processEntries);
+    $("#clear").addEventListener("click", clearEntries);
+    $("#miles").addEventListener("dblclick", clearEntries);
+    $("#miles").focus();
+});
