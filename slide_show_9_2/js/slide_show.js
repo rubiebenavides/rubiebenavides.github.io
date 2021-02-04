002bf07d91561b3c8b87c088c930d89610c27bba
Rubie Benavides <73657272+rubiebenavides@users.noreply.github.com>
2021-02-03 23:17:14 -0700

    Add files via upload
    Added Exercise 9-2 on 3 February 2021.

---
+++ slide_show_9_2/js/slide_show.js
@@ -0,0 +1,23 @@
+"use strict";
+$(document).ready( () => {
+    let nextSlide = $("#slides img:first-child");
+        
+    // start slide show
+    setInterval( () => {   
+        $("#caption").fadeOut(1000);
+        $("#slide").fadeOut(2000,
+            () => {
+                if (nextSlide.next().length == 0) {
+                    nextSlide = $("#slides img:first-child");
+                }
+                else {
+                    nextSlide = nextSlide.next();
+                }
+                const nextSlideSource = nextSlide.attr("src");
+                const nextCaption = nextSlide.attr("alt");
+                $("#slide").attr("src", nextSlideSource).slideDown(2000);                    
+                $("#caption").text(nextCaption).show(1000);
+            });    // end fadeOut()
+    },
+    5000);         // end setInterval()
+});
